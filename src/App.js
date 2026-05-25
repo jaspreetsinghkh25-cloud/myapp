bc7db1c0002e1b86e3457c6dffe51314a7949922
Jaspreet Singh <jaspreetsinghkh25@gmail.com>
2026-05-25 08:12:15 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import File from './File';
+import File2 from './File2';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/File' element={<File/>}/>
+          <Route path='/File2' element={<File2/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/File.js
@@ -0,0 +1,60 @@
+import { Add, Check, Circle, Delete } from "@mui/icons-material"
+import axios from "axios";
+import { useEffect, useEffectEvent, useState } from "react"
+export default function File(){
+    const [list, setlist]= useState([]);
+    const [name , setname]= useState("")
+
+    function Fun1(){
+        axios.get("http://localhost:6002/Todo").then((row)=>{
+            setlist(row.data)
+        })
+    }
+    function Fun2(){
+        
+        const ob = {name:name, status:false};
+        axios.post("http://localhost:6002/File3", ob).then((succ)=>{
+            console.log("Added")
+            // Fun1()
+        })
+        setlist([...list, ob])
+        
+        setname("")
+    }
+    function Delete1(i){
+    const newList = [...list];   
+    newList.splice(i, 1);   
+    setlist(newList);            
+}
+function Stats(i){
+    const newlist = [...list];
+    list[i].status=!list[i].status;
+    setlist(newlist)
+}
+    useEffect(()=>{
+        Fun1()
+    }, [])
+
+    return(
+        <>
+        <div className="col-lg-12 vh-100 bg-dark row justify-content-center align-items-center">
+            <div className="card col-lg-3">
+                <div className="card-header">
+                    <h1>Todo list</h1>
+                </div>
+                <div className="card-body text-start">
+                    <ul className="list-group">
+                        {list.map((row, i)=>(
+                            <li className="list-group-item m-1">{row.name}<button className="btn float-end" onClick={()=>Stats(i)}>{row.status ? <Check className="text-success"/>:<Circle className="text-warning"/>}</button><button className="btn text-danger float-end" onClick={()=>Delete1(i)}><Delete/></button></li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="card-footer input-group">
+                    <input type="text" value={name}  onChange={(e)=>setname(e.target.value)} className="form-control"/>
+                    <button className="btn btn-secondary" onClick={()=>Fun2()}><Add className=""/></button>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
