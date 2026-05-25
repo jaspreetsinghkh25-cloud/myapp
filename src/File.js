8cbaa528eb80a4686be1ddecf23aa267e0ca29fb
Jaspreet Singh <jaspreetsinghkh25@gmail.com>
2026-05-25 09:43:07 +0530

    working build

--- src/File.js
+++ src/File.js
@@ -1,5 +1,5 @@
 import { Add, Check, Circle, Delete } from "@mui/icons-material"
-import axios from "axios";
+// import axios from "axios";
 import { useEffect,  useState } from "react"
 export default function File(){
     const [list, setlist]= useState([]);
